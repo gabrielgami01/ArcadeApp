522cfefca51be77e48b93369a14f0775bc1256e5
Gabriel García Millán <gabriel.garciamillan01@gmail.com>
2024-06-21 12:44:02 +0200

    Initial Commit

---
+++ ArcadeApp/ArcadeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArcadeAppApp.swift
+//  ArcadeApp
+//
+//  Created by Gabriel Garcia Millan on 21/6/24.
+//
+
+import SwiftUI
+
+@main
+struct ArcadeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
