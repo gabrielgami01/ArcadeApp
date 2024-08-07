7338ee9f4ad8d74bdde63d1a47333c1b5baf3592
Gabriel García Millán <gabriel.garciamillan01@gmail.com>
2024-08-07 19:20:35 +0200

    New searchable views

---
+++ ArcadeApp/Views/SearchableView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchableView.swift
+//  ArcadeApp
+//
+//  Created by Gabriel Garcia Millan on 7/8/24.
+//
+
+import SwiftUI
+
+struct SearchableView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchableView()
+}
