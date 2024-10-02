32dba1a21ed542c9d52bdee35565c196626008ac
Gabriel García Millán <gabriel.garciamillan01@gmail.com>
2024-10-02 21:10:27 +0200

    JWTDecoder rollback

--- ArcadeApp/Extensions/JWTDecoder.swift
+++ ArcadeApp/Extensions/JWTDecoder.swift
@@ -1,25 +1,25 @@
 import Foundation
 
 struct JWTDecoder {
-    
-    private func base64Padding(jwt: String) -> String {
-        var encoded = jwt
-            .replacingOccurrences(of: "-", with: "+")
-            .replacingOccurrences(of: "_", with: "/")
-        let count = encoded.count % 4
-        for _ in 0 ..< count {
-            encoded += "="
-        }
-        return encoded
-    }
-    
+    // Método para verificar si un token JWT ha expirado
     func isTokenExpired(token: String) -> Bool {
+        // Separa el token en sus partes
         let segments = token.split(separator: ".")
         guard segments.count == 3 else {
-            return true
+            return true  // Si el token no tiene las tres partes, se considera inválido
+        }
+
+        // Decodifica la parte del payload (segunda parte)
+        let base64String = String(segments[1])
+            .replacingOccurrences(of: "-", with: "+")
+            .replacingOccurrences(of: "_", with: "/")
+        
+        var base64Length = base64String.count
+        while base64Length % 4 != 0 {
+            base64Length += 1
         }
         
-        let paddedBase64String = base64Padding(jwt: token)
+        let paddedBase64String = base64String.padding(toLength: base64Length, withPad: "=", startingAt: 0)
         
         guard let payloadData = Data(base64Encoded: paddedBase64String),
               let json = try? JSONSerialization.jsonObject(with: payloadData, options: []),
@@ -27,12 +27,13 @@ struct JWTDecoder {
             return true
         }
         
-        
+        // Extrae el valor de `exp` (tiempo de expiración)
         if let exp = payload["exp"] as? TimeInterval {
             let expirationDate = Date(timeIntervalSince1970: exp)
+            // Compara la fecha de expiración con la fecha actual
             return expirationDate <= Date()
         }
         
-        return true 
+        return true // Si no se encuentra `exp`, se considera que el token ha expirado
     }
 }
