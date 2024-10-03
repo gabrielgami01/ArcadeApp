08f0f2515bf6269254ab565b703163407e62a194
Gabriel García Millán <gabriel.garciamillan01@gmail.com>
2024-10-03 11:00:35 +0200

    JWTDecoder refactor

--- ArcadeApp/Extensions/JWTDecoder.swift
+++ ArcadeApp/Extensions/JWTDecoder.swift
@@ -1,39 +1,39 @@
 import Foundation
 
-struct JWTDecoder {
-    // Método para verificar si un token JWT ha expirado
-    func isTokenExpired(token: String) -> Bool {
-        // Separa el token en sus partes
-        let segments = token.split(separator: ".")
-        guard segments.count == 3 else {
-            return true  // Si el token no tiene las tres partes, se considera inválido
-        }
+struct JWTPayload: Codable {
+    let exp: TimeInterval?
+}
 
-        // Decodifica la parte del payload (segunda parte)
-        let base64String = String(segments[1])
+struct JWTDecoder {
+    private func base64Padding(jwt: String) -> String {
+        var encoded = jwt
             .replacingOccurrences(of: "-", with: "+")
             .replacingOccurrences(of: "_", with: "/")
-        
-        var base64Length = base64String.count
-        while base64Length % 4 != 0 {
-            base64Length += 1
+        let count = encoded.count % 4
+        for _ in 0 ..< count {
+            encoded += "="
         }
-        
-        let paddedBase64String = base64String.padding(toLength: base64Length, withPad: "=", startingAt: 0)
-        
-        guard let payloadData = Data(base64Encoded: paddedBase64String),
-              let json = try? JSONSerialization.jsonObject(with: payloadData, options: []),
-              let payload = json as? [String: Any] else {
+        return encoded
+    }
+    
+    func isTokenExpired(token: String) -> Bool {
+        let segments = token.split(separator: ".")
+        guard segments.count == 3 else {
             return true
         }
         
-        // Extrae el valor de `exp` (tiempo de expiración)
-        if let exp = payload["exp"] as? TimeInterval {
-            let expirationDate = Date(timeIntervalSince1970: exp)
-            // Compara la fecha de expiración con la fecha actual
-            return expirationDate <= Date()
+        let paddedBase64String = base64Padding(jwt: String(segments[1]))
+        
+        guard let payloadData = Data(base64Encoded: paddedBase64String) else {
+            return true
         }
         
-        return true // Si no se encuentra `exp`, se considera que el token ha expirado
+        let decoder = JSONDecoder()
+        if let payload = try? decoder.decode(JWTPayload.self, from: payloadData), let exp = payload.exp {
+                let expirationDate = Date(timeIntervalSince1970: exp)
+                return expirationDate <= Date()
+        }
+                
+        return true
     }
 }
